04d23594448cc4ab11f3654304318943e249351c
Juan Rosas <rosas.juan@live.com>
2023-04-22 17:34:38 -0300

    sidebar en home

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 let boton = document.getElementById("videoBtn");
 let video = document.getElementById("video-home");
+let hamburger = document.getElementsByClassName("hamburguer");
+let sidebar = document.getElementById("mySidebar");
+let closeBtn = document.getElementsByClassName("closeSidebarBtn");
 
 function pauseVideo() {
   if (video.paused) {
@@ -9,4 +12,12 @@ function pauseVideo() {
       video.pause();
       boton.innerHTML = "play";
   }
+}
+
+function openSidebar() {
+  sidebar.style.width = "250px";
+}
+
+function closeSidebar() {
+  sidebar.style.width = "0";
 }
